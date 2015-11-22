1d4cab27615e210b1b2d77d827e455e320faebe9
Laura Feier <feierlaura10@gmail.com>
2015-11-22 19:05:37 +0200

    new greeter message

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def greeter(request):
+    return HttpResponse("Hello light! Where's my HTML??")
