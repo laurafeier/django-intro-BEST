da8bfbdf62e143f4e98ec0d97f109663b069b4e0
Laura Feier <feierlaura10@gmail.com>
2015-11-22 20:25:03 +0200

    added post detail page

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -11,3 +11,8 @@ def post_list(request):
 
     all_posts = all_posts.order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': all_posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
