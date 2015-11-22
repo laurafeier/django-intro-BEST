92f3d6d4a8cde242b80a411b507ec90859c8b24d
Laura Feier <feierlaura10@gmail.com>
2015-11-22 22:29:10 +0200

    post can now be added

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -20,7 +20,17 @@ def post_detail(request, pk):
 
 
 def post_new(request):
-    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form = PostForm()
     return render(request, 'blog/post_new.html', {'form': form})
 
 
