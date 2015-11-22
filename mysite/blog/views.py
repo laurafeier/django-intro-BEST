ca483b0d5600995f6c4958e5442f2d82bd8358c1
Laura Feier <feierlaura10@gmail.com>
2015-11-22 20:05:48 +0200

    added posts ordering

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -8,4 +8,6 @@ def post_list(request):
         all_posts = Post.objects.all()
     else:
         all_posts = Post.objects.filter(published_date__lte=timezone.now())
+
+    all_posts = all_posts.order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': all_posts})
